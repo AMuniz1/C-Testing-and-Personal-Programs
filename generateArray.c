70d9ef500251b25204ef1e946ac046c55d4d476b
AMuniz1 <ams.tesl@gmail.com>
2022-06-17 10:40:56 -0300

    também atualizado

--- generateArray.c
+++ generateArray.c
@@ -20,7 +20,7 @@ int *generateArray(){
 
   printf("Enter %d elements: \n", SIZE);
   for(size_t i = 0; i < SIZE; i++){
-    printf("#%u: ", i+1);
+    printf("#%lu: ", i+1);
     scanf("%d", &myArr[i]);
   }
 
